90dca03818e4078c1a47492cbecb7fdd3622d236
t-lohani <t.lohani@gmail.com>
2017-02-06 02:29:16 -0500

    Longest Pallindrome

---
+++ Maths/LongestPallindrome.java
@@ -0,0 +1,34 @@
+/*
+ * Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * */
+
+package hashtable;
+
+import java.util.HashSet;
+
+public class LongestPallindrome {
+
+	public static void main(String[] args) {
+		String s = "aaaaabbbbbbcccccada";
+		System.out.println(longestPalindrome(s));
+	}
+	
+	public static int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
+            }else{
+                hs.add(s.charAt(i));
+            }
+        }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
+    }
+
+}
