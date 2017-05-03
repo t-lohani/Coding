600edee5374623314c4117e74c0d5e61bc71abc4
t-lohani <t.lohani@gmail.com>
2017-05-03 16:08:01 -0400

    Minimmum path sum

---
+++ Dynamic Proramming/MinimumPathSum.java
@@ -0,0 +1,36 @@
+/*
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+ * */
+
+package dynamicprogramming;
+
+public class MinimumPathSum {
+
+	public static void main(String[] args) {
+		int[][] grid = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}};
+		System.out.println(minPathSum(grid));
+	}
+
+	public static int minPathSum(int[][] grid) {
+        
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(i==0 || j==0) {
+                    if(i==0 && j==0)
+                        grid[i][j] = grid[i][j];
+                    else if(i==0)
+                        grid[i][j] = grid[i][j]+grid[i][j-1];
+                    else if(j==0)
+                        grid[i][j] = grid[i][j]+grid[i-1][j];
+                }
+                else
+                    grid[i][j] = grid[i][j-1]<grid[i-1][j]?(grid[i][j]+grid[i][j-1]):(grid[i][j]+grid[i-1][j]);
+            }
+        }
+        
+        return grid[grid.length-1][grid[0].length-1];
+    }
+
+}
