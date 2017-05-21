c65bc734d088fd649d6d99874d2f3a972595edde
t-lohani <t.lohani@gmail.com>
2017-05-20 22:22:57 -0400

    Add Strings

---
+++ Maths/AddStrings.java
@@ -0,0 +1,35 @@
+/*
+ * Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Note:
+
+    The length of both num1 and num2 is < 5100.
+    Both num1 and num2 contains only digits 0-9.
+    Both num1 and num2 does not contain any leading zero.
+    You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ * */
+
+package maths;
+
+public class AddStrings {
+
+	public static void main(String[] args) {
+		String num1 = "123";
+		String num2 = "4567";
+		
+		System.out.println(addStrings(num1, num2));
+	}
+
+	public static String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        return sb.reverse().toString();
+    }
+}
