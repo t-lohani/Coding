ceff3a17a5734423417c0b7a6c7dd6e000d6f54d
Tarun Lohani <t.lohani@gmail.com>
2017-09-15 23:25:57 -0400

    Implement Strstr

---
+++ Strings/ImplementStrStr.java
@@ -0,0 +1,25 @@
+package strings;
+
+public class ImplementStrStr {
+	public static void main(String[] args) {
+		String haystack = "";
+		String needle = "";
+		System.out.println(strStr(haystack, needle));
+	}
+
+	public static int strStr(String haystack, String needle) {
+        int l1 = haystack.length(), l2 = needle.length();
+        if (l1 < l2) {
+            return -1;
+        } else if (l2 == 0) {
+            return 0;
+        }
+        int threshold = l1 - l2;
+        for (int i = 0; i <= threshold; ++i) {
+            if (haystack.substring(i,i+l2).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
