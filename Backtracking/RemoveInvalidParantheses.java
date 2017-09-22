601409bfcff681ef72828e8aef8d7e207a47cece
Tarun Lohani <t.lohani@gmail.com>
2017-09-21 21:26:23 -0400

    Remove Invalid Parantheses

---
+++ Backtracking/RemoveInvalidParantheses.java
@@ -0,0 +1,37 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveInvalidParantheses {
+
+	public static void main(String[] args) {
+		List<String> ans = removeInvalidParentheses(")(");
+		for (int i=0; i<ans.size(); i++) {
+			System.out.println(ans.get(i));
+		}
+	}
+	
+	public static List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList<>();
+        remove(s, ans, 0, 0, new char[]{'(', ')'});
+        return ans;
+    }
+    
+    public static void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+        for (int stack = 0, i = last_i; i < s.length(); ++i) {
+            if (s.charAt(i) == par[0]) stack++;
+            if (s.charAt(i) == par[1]) stack--;
+            if (stack >= 0) continue;
+            for (int j = last_j; j <= i; ++j)
+                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+            return;
+        }
+        String reversed = new StringBuilder(s).reverse().toString();
+        if (par[0] == '(')
+            remove(reversed, ans, 0, 0, new char[]{')', '('});
+        else
+            ans.add(reversed);
+    }
+}
