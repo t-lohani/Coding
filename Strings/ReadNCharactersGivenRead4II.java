ca42e89c3f00dcfc1a597a2cc20dbaabf3c3764b
Tarun Lohani <t.lohani@gmail.com>
2017-09-29 06:08:54 -0400

    Read N Characters Given Read4 II

---
+++ Strings/ReadNCharactersGivenRead4II.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class ReadNCharactersGivenRead4II {
+
+	int buffPtr = 0;
+    int buffCnt = 0;
+    char[] buff = new char[4];
+    
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+}
