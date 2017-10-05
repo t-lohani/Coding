2a3c9b647646df75c30f2b9fa99793ab8e357e5d
Tarun Lohani <t.lohani@gmail.com>
2017-10-05 19:17:59 -0400

    Binary Tree Longest Consecutive Sequence II

---
+++ Tree/BinaryTreeLongestConsecutiveSequenceII.java
@@ -0,0 +1,31 @@
+package tree;
+
+public class BinaryTreeLongestConsecutiveSequenceII {
+
+	int maxval = 0;
+    public int longestConsecutive(TreeNode root) {
+        postOrder(root);
+        return maxval;
+    }
+    public int[] postOrder(TreeNode root) {
+        if (root == null)
+            return new int[] {0,0};
+        int inr = 1, dcr = 1;
+        if (root.left != null) {
+            int[] l = postOrder(root.left);
+            if (root.val == root.left.val + 1)
+                dcr = l[1] + 1;
+            else if (root.val == root.left.val - 1)
+                inr = l[0] + 1;
+        }
+        if (root.right != null) {
+            int[] r = postOrder(root.right);
+            if (root.val == root.right.val + 1)
+                dcr = Math.max(dcr, r[1] + 1);
+            else if (root.val == root.right.val - 1)
+                inr = Math.max(inr, r[0] + 1);
+        }
+        maxval = Math.max(maxval, dcr + inr - 1);
+        return new int[] {inr, dcr};
+    }
+}
