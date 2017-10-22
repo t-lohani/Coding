ba16b585e7c472f1e700172a7c546cb0b21054b8
Tarun Lohani <t.lohani@gmail.com>
2017-10-22 04:16:10 -0400

    Subsets

---
+++ Backtracking/Subsets.java
@@ -0,0 +1,46 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Subsets {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3};
+		subsets(nums);
+		
+		for (int i=0; i<answer.size(); i++) {
+			for (int j=0; j<answer.get(i).size(); j++) {
+				System.out.print(answer.get(i).get(j));
+			}
+			System.out.println();
+		}
+	}
+
+	static List<List<Integer>> answer = new ArrayList<>();
+    
+    public static List<List<Integer>> subsets(int[] nums) {
+        
+        List<Integer> list = new LinkedList<Integer>();
+        generate(nums, 0, list);
+        
+        return answer;
+    }
+    
+    public static void generate(int[] numArr, int index, List<Integer> arrList) {
+        
+        if(index==numArr.length) {
+            List<Integer> list = new LinkedList<Integer>();
+            for(int i=0; i<arrList.size(); i++) {
+                list.add(arrList.get(i));
+            }
+            answer.add(list);
+            return;
+        }
+        arrList.add(numArr[index]);
+        generate(numArr, index+1, arrList);
+        arrList.remove(arrList.size()-1);
+        generate(numArr, index+1, arrList);
+    }
+}
