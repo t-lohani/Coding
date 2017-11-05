2513b6690c5ad4f709bcbd1c309e29e3834636a2
Tarun Lohani <t.lohani@gmail.com>
2017-11-04 20:35:17 -0400

    Integer to Roman

---
+++ Strings/IntegerToRoman.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class IntegerToRoman {
+
+	public static void main(String[] args) {
+		int num = 3234;
+		System.out.println(intToRoman(num));
+	}
+
+	public static String intToRoman(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
